025d083b625ced3351f5cdd9db4b35c715dacbc1
Max Vagner <max.vagner@worldnomadsgroup.com>
2018-02-15 16:40:31 +1100

    Initial commit

---
+++ Helpers/HttpClientHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    public class HttpClientHandler : IHttpHandler
+    {
+        private readonly HttpClient _client = new HttpClient();
+
+        public HttpResponseMessage Send(HttpRequestMessage requestMessage)
+        {
+            var servicePoint = ServicePointManager.FindServicePoint(requestMessage.RequestUri);
+            servicePoint.ConnectionLeaseTimeout = 60 * 1000; // 1 minute
+
+            var response = _client.SendAsync(requestMessage);
+            return response.Result;
+        }
+
+        public HttpResponseMessage Get(string url, IDictionary<string, string> headers)
+        {
+            return Send(BuildRequestMessage(HttpMethod.Get, url, headers));
+        }
+
+        private HttpRequestMessage BuildRequestMessage(HttpMethod httpMethod, string url, IDictionary<string, string> headers)
+        {
+            var message = new HttpRequestMessage(httpMethod, url);
+
+            if (headers != null && headers.Count > 0)
+            {
+                foreach(var header in headers)
+                {
+                    message.Headers.Add(header.Key, header.Value);
+                }
+            }
+
+            return message;
+        }
+    }
+}
